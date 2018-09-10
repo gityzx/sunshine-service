b9b1b67c149ca09133d6b81d4804a2ff38bb7a78
yangzhaoxu <wecash@yangzhaoxu.local>
2018-09-10 19:07:51 +0800

    sunshine-service

---
+++ sunshine-service-java/src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @Description:
+ * @Date: 2018/9/10 19:00
+ * @Auther: yangzhaoxu
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("fdsfds");
+    }
+}
